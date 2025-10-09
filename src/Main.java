4137191a145596d128a31cb7436b5e7f70fb249d
Mateus Prado <maateussp@gmail.com>
2025-10-09 13:52:00 -0300

    Resolve conflito em Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
-<<<<<<< Updated upstream
-=======
 import entity.Produto;
 import service.Ecomerceservice;
 
-
 import java.util.ArrayList;
 import java.util.List;
->>>>>>> Stashed changes
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< Updated upstream
-=======
         Ecomerceservice ecomerceservice = new Ecomerceservice();
         ecomerceservice.addProduto();
         ecomerceservice.getProdutoslist();
-
->>>>>>> Stashed changes
-
-
     }
 }
