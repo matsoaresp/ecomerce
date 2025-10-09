82ef4c4e25a2849b74942c0affd095a99e98cc56
Mateus Prado <maateussp@gmail.com>
2025-10-09 19:01:30 -0300

    Additing utils and toString to application

--- src/service/Ecomerceservice.java
+++ src/service/Ecomerceservice.java
@@ -3,60 +3,21 @@ package service;
 import entity.Eletronico;
 import entity.Produto;
 
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static utils.InputUtils.*;
 
 public class Ecomerceservice {
 
     private static final Logger logger = Logger.getLogger(Ecomerceservice.class.getName());
-    private List<Produto> produtoslist;
+    private final List<Produto> produtoslist;
 
     public Ecomerceservice() {
         this.produtoslist = new ArrayList<>();
-
-        // Configuração do logger
-        Logger rootLogger = Logger.getLogger("");
-        rootLogger.setLevel(Level.ALL);
-
-        for (var handler : rootLogger.getHandlers()) {
-            rootLogger.removeHandler(handler);
-        }
-
-        ConsoleHandler handler = new ConsoleHandler() {
-            @Override
-            protected synchronized void setOutputStream(OutputStream out) throws SecurityException {
-                super.setOutputStream(System.out);
-            }
-        };
-        handler.setLevel(Level.ALL);
-        rootLogger.addHandler(handler);
-    }
-
-    public Ecomerceservice(List<Produto> produtos) {
-        this.produtoslist = produtos;
-
-        // Configuração do logger
-        Logger rootLogger = Logger.getLogger("");
-        rootLogger.setLevel(Level.ALL);
-
-        for (var handler : rootLogger.getHandlers()) {
-            rootLogger.removeHandler(handler);
-        }
-
-        ConsoleHandler handler = new ConsoleHandler() {
-            @Override
-            protected synchronized void setOutputStream(OutputStream out) throws SecurityException {
-                super.setOutputStream(System.out);
-            }
-        };
-        handler.setLevel(Level.ALL);
-        rootLogger.addHandler(handler);
     }
 
     public void addProduto() {
@@ -66,10 +27,8 @@ public class Ecomerceservice {
             System.out.println("Quantos produtos deseja inserir?");
             int quantidadeProdutos = sc.nextInt();
             sc.nextLine();
-
             for (int i = 1; i <= quantidadeProdutos; i++) {
                 System.out.println("\n--- " + i + "º Produto ---");
-
                 String nome = getMessage(sc, "Nome do produto:");
                 int quantidade = getIntMessage(sc, "Quantidade do produto:");
                 double preco = getDoubleMessage(sc, "Preco do produto:");
@@ -81,14 +40,13 @@ public class Ecomerceservice {
 
         } catch (InputMismatchException e) {
             logger.severe("\u001B[31m[ERROR] Entrada invalida. Por favor, insira os dados corretamente: " + e.getMessage() + "\u001B[0m");
-            e.printStackTrace();
+
         } catch (Exception e) {
             logger.severe("\u001B[31m[ERROR] Erro ao adicionar produtos: " + e.getMessage() + "\u001B[0m");
-            e.printStackTrace();
+
         }
     }
-
-    public List<Produto> getProdutoslist() {
+    public List<Produto> getProdutos() {
         if (produtoslist == null || produtoslist.isEmpty()) {
             logger.warning("\u001B[33m[WARNING] A lista de produtos esta vazia.\u001B[0m");
         } else {
@@ -104,31 +62,9 @@ public class Ecomerceservice {
         return produtoslist;
     }
 
-    public String getMessage(Scanner sc, String mensagem) {
-        System.out.println(mensagem);
-        return sc.nextLine();
-    }
 
-    public int getIntMessage(Scanner sc, String mensagem) {
-        while (true) {
-            try {
-                System.out.println(mensagem);
-                return Integer.parseInt(sc.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Entrada invalida. Por favor, insira um numero inteiro.");
-            }
-        }
-    }
 
-    public double getDoubleMessage(Scanner sc, String mensagem) {
-        while (true) {
-            try {
-                System.out.println(mensagem);
-                String input = sc.nextLine().replace(",", ".");
-                return Double.parseDouble(input);
-            } catch (NumberFormatException e) {
-                System.out.println("Entrada invalida. Por favor, insira um numero decimal.");
-            }
-        }
-    }
+
+
+
 }
